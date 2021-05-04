df2d205d9012b8e341f88b798edd9495ad88ca52
Bas Martens <bmartens1988@kpnmail.nl>
2021-05-04 22:26:39 +0200

    Data-driven Tests

---
+++ GameEngine.Tests/HealthDamageDataAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace GameEngine.Tests
+{
+    public class HealthDamageDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { 0, 100 };
+            yield return new object[] { 1, 99 };
+            yield return new object[] { 50, 50 };
+            yield return new object[] { 75, 25 };
+            yield return new object[] { 101, 1 };
+
+            // Custom DataAttribute can also be used in conjunction with external data
+            // i.e. reading test data from external file through usage of this attribute
+        }
+    }
+}
