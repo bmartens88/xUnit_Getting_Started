ab27893cd9b66e7e4a10f76cd6a729594d2069ee
Bas Martens <bmartens1988@kpnmail.nl>
2021-05-04 10:34:55 +0200

    Initial commit of code

---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GameEngine.Tests
+{
+  public class PlayerCharacterShould
+  {
+    [Fact]
+    public void BeInexperiencedWhenNew()
+    {
+      PlayerCharacter sut = new PlayerCharacter();
+    }
+  }
+}
